f3999ade5b787679703d2ebfc683f23f7aa5427b
MARIOPROPIOS <mariopropiosplaza@gmail.com>
2026-09-23 12:37:04 +0200

    final de clase, subo archivos actualizados

---
+++ UT2/programas_02/js/ejercicioPin.js
@@ -0,0 +1,6 @@
+const digito1 = Math.floor(Math.random()*10);
+const digito2 = Math.floor(Math.random()*10);
+const digito3 = Math.floor(Math.random()*10);
+const digito4 = Math.floor(Math.random()*10);
+
+alert(`Pin generado= ${digito1}${digito2}${digito3}${digito4}`);
